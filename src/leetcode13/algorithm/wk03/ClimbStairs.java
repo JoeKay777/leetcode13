5a77131dd3363a54a9558c084cdc4ca5ef734728
qkh <qkhlife@163.com>
2020-08-13 10:13:58 +0800

    70. 爬楼梯

--- src/leetcode13/algorithm/wk03/ClimbStairs.java
+++ src/leetcode13/algorithm/wk03/ClimbStairs.java
@@ -8,10 +8,7 @@ package leetcode13.algorithm.wk03;
 public class ClimbStairs {
 	// f(n) = f(n-1) + f(n-2);
 	public int climbStairs(int n) {
-		if(n < 3) {
-			return n;
-		}
-		int f1 = 1, f2 = 2, fn = 0;
+		int f1 = 1, f2 = 2, fn = n;
 		while(n > 2) {
 			fn = f1 + f2;
 			f1 = f2;
@@ -24,5 +21,7 @@ public class ClimbStairs {
 	public static void main(String[] args) {
 		int climbStairs = new ClimbStairs().climbStairs(5);
 		System.out.println(climbStairs);
+		
+		System.out.println(132213213/100%10);	// 获取一个整数的百位值
 	}
 }

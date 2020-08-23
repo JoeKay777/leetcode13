9f484ada35f338e999659808ee25b4c633d70ebd
JoeKay777 <qkhlife@163.com>
2020-08-23 22:13:38 +0800

    wk04

--- src/leetcode13/algorithm/wk04/StudySummary.java
+++ src/leetcode13/algorithm/wk04/StudySummary.java
@@ -2,7 +2,7 @@ package leetcode13.algorithm.wk04;
 
 /**
  * 学习总结：
- * 使用二分查找，寻找一个半有序数组 [4, 5, 6, 7, 0, 1, 2] 中间无序的地方
+ * 使用二分查找，寻找一个半有序数组 [4, 5, 6, 7, 0, 1, 2] 中间无序的地方,最小值即为无序的地方
  * // Java
  * public int binarySearch(int[] array, int target) {
  * int left = 0, right = array.length - 1, mid;
@@ -31,7 +31,7 @@ public class StudySummary {
             } else if (nums[mid] > nums[left]) { // 最小值在右边
                 left = mid;
             } else {
-                // 中间值等于其中一个，最小值为其中小值
+                // 剩余两个值时，最小值为其中小值
                 left = nums[left] > nums[right] ? right : left;
                 break;
             }

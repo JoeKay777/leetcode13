f8c7fa31fdd6d3ab51faeada851fec12c0f92dd1
JoeKay777 <qkhlife@163.com>
2020-08-23 18:12:14 +0800

    wk04

--- src/leetcode13/algorithm/wk04/Problem_153.java
+++ src/leetcode13/algorithm/wk04/Problem_153.java
@@ -7,7 +7,7 @@ public class Problem_153 {
 
     public int findMin(int[] nums) {
         int left = 0, right = nums.length - 1;
-        while (left <= right) {
+        while (left < right) { // right 不能等于left，当仅剩两个值时，left值 小于 right值时，此时mid = left会死循环
             int mid = (right - left) / 2 + left;
             if (nums[mid] > nums[right]) {   // 右侧旋转，最小值在右侧且mid不可能为最小值
                 left = mid + 1;

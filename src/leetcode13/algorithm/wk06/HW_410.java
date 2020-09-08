70051300f5c6e6e8f8d690c6e367ad3b6a42cdc6
JoeKay777 <qkhlife@163.com>
2020-09-08 10:27:41 +0800

    wk07

--- src/leetcode13/algorithm/wk06/HW_410.java
+++ src/leetcode13/algorithm/wk06/HW_410.java
@@ -1,5 +1,7 @@
 package leetcode13.algorithm.wk06;
 
+import java.util.Arrays;
+
 public class HW_410 {
 
     /**
@@ -27,7 +29,7 @@ public class HW_410 {
                 sub += num;
                 if (sub > mid) { // 大于猜想的分段和，则分段
                     cnt++;
-                    if(cnt > m) {
+                    if (cnt > m) {
                         break;
                     }
                     sub = num;
@@ -40,5 +42,36 @@ public class HW_410 {
             }
         }
         return left;
+    }
+
+    /**
+     * 解法二：动态规划
+     * dp[i][j]: 0-j元素 分隔i段各自和的最大值最小
+     * <p>
+     * dp[i][j] = min(dp[i][j], max(dp[i-1][k], sum[j] - sum[k])) ;  i-1 <= k < j
+     *
+     * @param nums
+     * @param m
+     * @return
+     */
+    public int splitArray_2(int[] nums, int m) {
+        int[] sum = new int[nums.length + 1];   // 前i和
+        for (int i = 0; i < nums.length; i++) {
+            sum[i + 1] = sum[i] + nums[i];
+        }
+        int[][] dp = new int[m + 1][nums.length + 1];
+        // 填充默认值，为最大值
+        for (int[] col : dp) {
+            Arrays.fill(col, Integer.MAX_VALUE);
+        }
+        dp[0][0] = 0;   // 保证分1段时，公式满足
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= nums.length; j++) {
+                for (int k = i - 1; k < j; k++) {
+                    dp[i][j] = Math.min(dp[i][j], Math.max(dp[i - 1][k], sum[j] - sum[k]));
+                }
+            }
+        }
+        return dp[m][nums.length];
     }
 }

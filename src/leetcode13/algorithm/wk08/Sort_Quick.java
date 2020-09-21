fe1e21fe99711955461735b0bbca7f256ab21e9f
JoeKay777 <qkhlife@163.com>
2020-09-21 14:26:36 +0800

    wk08

--- src/leetcode13/algorithm/wk08/Sort_Quick.java
+++ src/leetcode13/algorithm/wk08/Sort_Quick.java
@@ -5,19 +5,20 @@ package leetcode13.algorithm.wk08;
  */
 public class Sort_Quick {
 
+    // 方法一
     public static void quickSort(int[] arr, int left, int right) {
-        if(left >= right) return;
+        if (left >= right) return;
         int pivot = arr[left];  // 基准点
         int i = left;
         int j = right;
         while (i < j) {
-            while (arr[j] >= pivot && i < j) {      // 优先从左边走，保证相遇点是小于基准点的
+            while (arr[j] >= pivot && i < j) {      // 优先从右边走，保证相遇点是小于基准点的
                 j--;
             }
-            while (arr[i] <= pivot && i <j) {
+            while (arr[i] <= pivot && i < j) {
                 i++;
             }
-            if(i <j) {
+            if (i < j) {
                 int temp = arr[i];
                 arr[i] = arr[j];
                 arr[j] = temp;
@@ -25,13 +26,32 @@ public class Sort_Quick {
         }
         arr[left] = arr[i];
         arr[i] = pivot;
-        quickSort(arr, left, i -1);
+        quickSort(arr, left, i - 1);
+        quickSort(arr, i + 1, right);
+    }
+
+    // 方法二
+    public static void quickSort_2(int[] arr, int left, int right) {
+        if (left >= right) return;
+        int pivot = arr[left], i = left, j = right;
+        while (i < j) {
+            while (arr[j] >= pivot && i < j) {
+                j--;
+            }
+            arr[i] = arr[j];
+            while (arr[i] <= pivot && i < j) {
+                i++;
+            }
+            arr[j] = arr[i];
+        }
+        arr[i] = pivot;
+        quickSort(arr, left, i - 1);
         quickSort(arr, i + 1, right);
     }
 
     public static void main(String[] args) {
-        int[] arr = {10,7,2,4,7,62,3,4,2,1,8,9,19};
-        quickSort(arr, 0, arr.length-1);
+        int[] arr = {10, 7, 2, 4, 7, 62, 3, 4, 2, 1, 8, 9, 19};
+        quickSort_2(arr, 0, arr.length - 1);
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
         }

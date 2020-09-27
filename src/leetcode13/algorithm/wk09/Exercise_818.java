73ff538ee2f068cd41e92888d507a0bb85ebbf2f
JoeKay777 <qkhlife@163.com>
2020-09-27 16:18:18 +0800

    wk09

--- src/leetcode13/algorithm/wk09/Exercise_818.java
+++ src/leetcode13/algorithm/wk09/Exercise_818.java
@@ -29,27 +29,5 @@ public class Exercise_818 {
         return dp[target];
     }
 
-    public static void main(String[] args) {
-//        int[] dp = new int[target + 3];
-//        Arrays.fill(dp, Integer.MAX_VALUE);
-//        dp[0] = 0;
-//        dp[1] = 1;
-//        dp[2] = 4;
-//
-//        for (int t = 3; t <= target; ++t) {
-//            int k = 32 - Integer.numberOfLeadingZeros(t);
-//            if (t == (1 << k) - 1) {
-//                dp[t] = k;
-//                continue;
-//            }
-//            for (int j = 0; j < k - 1; ++j)
-//                dp[t] = Math.min(dp[t], dp[t - (1 << (k - 1)) + (1 << j)] + k - 1 + j + 2);
-//            if ((1 << k) - 1 - t < t)
-//                dp[t] = Math.min(dp[t], dp[(1 << k) - 1 - t] + k + 1);
-//        }
-//
-//        return dp[target];
-        new Exercise_818().racecar(2);
 
-    }
 }
